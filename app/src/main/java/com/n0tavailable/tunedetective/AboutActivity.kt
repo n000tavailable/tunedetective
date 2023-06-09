7acd2aa11292671c187507f06038305ab708d60e
user <jonassold@tuta.io>
2023-06-09 05:51:54 +0200

    Added additional vertical space in activity_main.xml.

--- app/src/main/java/com/n0tavailable/tunedetective/AboutActivity.kt
+++ app/src/main/java/com/n0tavailable/tunedetective/AboutActivity.kt
@@ -20,30 +20,6 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
-        val spinner: Spinner = findViewById(R.id.spinner)
-        val options = arrayOf("About", "Home")
-
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinner.adapter = adapter
-
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                val selectedItem = parent.getItemAtPosition(position) as String
-                if (selectedItem == "Home") {
-                    val intent = Intent(this@AboutActivity, MainActivity::class.java)
-                    startActivity(intent)
-
-                    // Reset the spinner selection
-                    spinner.setSelection(0)
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // Do nothing
-            }
-        }
-
         val profileImageView: ImageView = findViewById(R.id.profile)
         Glide.with(this)
             .load(R.drawable.profile)
